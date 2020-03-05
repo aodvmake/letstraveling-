3d230792a2d13f7ab4f5f6c7538490beb3f56342
gaby-2020 <61641164+gaby-2020@users.noreply.github.com>
2020-03-04 18:32:06 -0600

    cambio

--- views/updateGuia.php
+++ views/updateGuia.php
@@ -22,159 +22,154 @@ include("../controllers/session.php");
 	<title></title>
 </head>
 <body>
-<!--Navbar -->
-<nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
-    aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home
-          <span class="sr-only">(current)</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">Dropdown
-        </a>
-        <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
-          <a class="dropdown-item" href="guiahorario.php">Crear Guia</a>
-          <a class="dropdown-item" href="guiacon.php">Ver Horario</a>
-          <a class="dropdown-item" href="#">Something else here</a>
+  <div class="container">
+  <!--Navbar -->
+      <nav class="mb-1 navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">LET´S TRAVEL</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+          aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="guia.php">HOME<span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">HORARIOS
+              </a>
+              <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                <a class="dropdown-item" href="guiahorario.php">CREAR HORARIO</a>
+                <a class="dropdown-item" href="guiacon.php">VER HORARIOS</a>
+              </div>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto nav-flex-icons">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right dropdown-default"
+                aria-labelledby="navbarDropdownMenuLink-333">
+                <a class="dropdown-item" href="updateGuia.php">CONFIGURACIÓN</a>
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#" onclick="location.href='../cerrarsession.php';">SALIR</a>
+              </div>
+            </li>
+          </ul>
         </div>
-      </li>
-    </ul>
-    <ul class="navbar-nav ml-auto nav-flex-icons">
-      <li class="nav-item">
-        <a class="nav-link waves-effect waves-light">
-          <i class="fab fa-twitter"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link waves-effect waves-light">
-          <i class="fab fa-google-plus-g"></i>
-        </a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-user"></i>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right dropdown-default"
-          aria-labelledby="navbarDropdownMenuLink-333">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="updateGuia.php">Configuracion</a>
-          <a class="dropdown-item" href="#">Something else here</a>
+      </nav>
+      <!--/.Navbar -->
+
+      <div class="content">
+        <div class="card-body px-lg-5 pt-0">
+            <h2>ACTUALIZAR MI INFORMACIÓN</h2>
+             <form method="POST" action="../controllers/actualizar.php">
+              <table>
+                <tr>
+                <td>
+                  <div class="row">
+                          <div class="col">
+                             <div class="md-form">
+                              <input class="form-control" type="password" name="pass" id="pass">
+                             <label for="pass">Password</label>
+                            </div>      
+                          </div>
+                    </div>
+                </td>
+                <td>
+               <button type="submit" class="btn aqua-gradient" name="actpass" id="actpass">Actualizar</button>  
+                </td>
+                <tr>  
+                <td>
+                      <div class="row">
+                            <div class="col">
+                            <div class="md-form">
+                             <input class="form-control" type="text" name="telefono" id="telefono" onkeypress="return soloNumeros(event);">
+                            <label for="telefono">Telefono</label>
+                          </div>      
+                       </div>
+                    </div>
+                </td>
+                <td>
+                  <button class="btn aqua-gradient" type="submit" name="actphone" id="actphone">Actualizar</button>  
+                </td>  
+                <tr>
+                <td>
+                      <div class="row">
+                          <div class="col">
+                              <div class="md-form">
+                               <input class="form-control" type="text"  name="domicilio" id="domicilio">
+                               <label for="domicilio">Calle</label>
+                              </div>      
+                            </div>
+                          </div>
+                </td>
+              <td>
+                <button class="btn aqua-gradient" type="submit" name="actdomi" id="actdomi">Actualizar</button> 
+              </td>  
+              </tr>
+                <td>
+                  <div class="row">
+                            <div class="col">
+                              <div class="md-form">
+                                 <input class="form-control" type="text" name="colonia" id="colonia">
+                                 <label for="colonia">Colonia</label>
+                              </div>      
+                            </div>
+                  </div>
+                </td>
+                <td>
+                  <button class="btn aqua-gradient" type="submit" name="actcol" id="actcol">Actualizar</button>  
+                </td> 
+              </tr>
+              <tr>
+                <td>
+                   <div class="row">
+                      <div class="col">
+                              <div class="md-form">
+                                 <input class="form-control" type="text"  name="ndomicilio" id="ndomicilio" onkeypress="return soloNumeros(event);">
+                                 <label for="foto">#</label>              
+                              </div>      
+                        </div>
+                   </div>
+                <td>
+                  <button class="btn aqua-gradient" type="submit" name="actndom" id="actndom">Actualizar</button>  
+                </td> 
+              </tr>  
+              <tr>
+                <td>
+                     <div class="row">
+                            <div class="col">
+                            <div class="md-form">
+                             <input class="form-control" type="text" name="municipio" id="municipio">
+                             <label for="Municipio">Municipio</label>
+                            </div>      
+                            </div>
+                          </div>
+                 </td> 
+                 <td>
+                  <button class="btn aqua-gradient" type="submit" name="actmun" id="actmun" >Actualizar</button>  
+                 </td> 
+              </tr> 
+              </table>
+          </form>
         </div>
-      </li>
-    </ul>
+      </div>
+
   </div>
-</nav>
-<!--/.Navbar -->
+    
 
-Bienvenido a guia
-<button type="button" class="btn btn-danger btn-rounded" onclick="location.href='../cerrarsession.php';">Cerrar session</button>
-   <form method="POST" action="../controllers/actualizar.php">
-<table>
-  <tr>
-  <td>
-    <div class="row">
-            <div class="col">
-               <div class="md-form">
-                <input class="form-control" type="password" name="pass" id="pass">
-               <label for="pass">Password</label>
-              </div>      
-            </div>
-      </div>
-  </td>
-  <td>
- <button type="submit" class="btn aqua-gradient" name="actpass" id="actpass">Actualizar</button>  
-  </td>
-  <tr>  
-  <td>
-        <div class="row">
-              <div class="col">
-              <div class="md-form">
-               <input class="form-control" type="text" name="telefono" id="telefono" onkeypress="return soloNumeros(event);">
-              <label for="telefono">Telefono</label>
-            </div>      
-         </div>
-      </div>
-  </td>
-  <td>
-    <button class="btn aqua-gradient" type="submit" name="actphone" id="actphone">Actualizar</button>  
-  </td>  
-  <tr>
-  <td>
-        <div class="row">
-            <div class="col">
-                <div class="md-form">
-                 <input class="form-control" type="text"  name="domicilio" id="domicilio">
-                 <label for="domicilio">Calle</label>
-                </div>      
-              </div>
-            </div>
-  </td>
-<td>
-  <button class="btn aqua-gradient" type="submit" name="actdomi" id="actdomi">Actualizar</button> 
-</td>  
-</tr>
-  <td>
-    <div class="row">
-              <div class="col">
-                <div class="md-form">
-                   <input class="form-control" type="text" name="colonia" id="colonia">
-                   <label for="colonia">Colonia</label>
-                </div>      
-              </div>
-    </div>
-  </td>
-  <td>
-    <button class="btn aqua-gradient" type="submit" name="actcol" id="actcol">Actualizar</button>  
-  </td> 
-</tr>
-<tr>
-  <td>
-     <div class="row">
-        <div class="col">
-                <div class="md-form">
-                   <input class="form-control" type="text"  name="ndomicilio" id="ndomicilio" onkeypress="return soloNumeros(event);">
-                   <label for="foto">#</label>              
-                </div>      
-          </div>
-     </div>
-  <td>
-    <button class="btn aqua-gradient" type="submit" name="actndom" id="actndom">Actualizar</button>  
-  </td> 
-</tr>  
-<tr>
-  <td>
-       <div class="row">
-              <div class="col">
-              <div class="md-form">
-               <input class="form-control" type="text" name="municipio" id="municipio">
-               <label for="Municipio">Municipio</label>
-              </div>      
-              </div>
-            </div>
-   </td> 
-   <td>
-    <button class="btn aqua-gradient" type="submit" name="actmun" id="actmun" >Actualizar</button>  
-   </td> 
-</tr> 
-</table>
-                    
 
-</form>
 </body>
 
 <script type="text/javascript" src="../lib/js/jquery.min.js"></script>
