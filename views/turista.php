dd19cd2d4eb66e65ebd2fed91863cce0e2688a61
gaby-2020 <61641164+gaby-2020@users.noreply.github.com>
2020-03-10 19:42:09 -0600

    cambio

--- views/turista.php
+++ views/turista.php
@@ -19,8 +19,9 @@ include("../controllers/horarioparaturista.php");
   <link rel="stylesheet" href="../lib/css/mdb.min.css">
   <!-- Your custom styles (optional) -->
   <link rel="stylesheet" href="../lib/css/style.css">
+  <link rel="icon" type="image/png" href="../lib/img/icono.png">
 <head>
-  <title></title>
+  <title>LET´S TRAVEL</title>
 </head>
 <body>
 <div class="container">
@@ -48,44 +49,57 @@ include("../controllers/horarioparaturista.php");
               aria-haspopup="true" aria-expanded="false">HORARIOS
             </a>
             <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
-              <a class="dropdown-item" href="guiahorario.php">CREAR HORARIOS</a>
+              <a class="dropdown-item" href="guiahorario.php">CREAR HORARIO</a>
               <a class="dropdown-item" href="guiacon.php">VER HORARIOS</a>
             </div>
           </li>
         </ul>
         <ul class="navbar-nav ml-auto nav-flex-icons">
-          <li class="nav-item">
-            <a class="nav-link waves-effect waves-light">
-              <i class="fas fa-times" aria-hidden="true" onclick="location.href='../cerrarsession.php';"></i>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-user"></i>
             </a>
+            <div class="dropdown-menu dropdown-menu-right dropdown-default"
+              aria-labelledby="navbarDropdownMenuLink-333">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#" onclick="location.href='../cerrarsession.php';">SALIR</a>
+            </div>
           </li>
         </ul>
       </div>
     </nav>
     <!--/.Navbar -->
 
- 
- Agregar horario
- <form  class="text-center" method="POST"> 
-<<<<<<< HEAD
-   <div class="table-responsive">
-       <table class="table">
-                    <tr>
-                      <th scope="col">DÍA</th>
-                      <th scope="col">HORA DE INICIO</th>
-                      <th scope="col">HORA DE FIN</th>
-                    </tr>
-                       <?php foreach ($horario as $a): ?>
-                        <tr>
-                          <td><?php echo $a['dia']?></td>
-                          <td><?php echo $a['horarioi']?></td>
-                          <td><?php echo $a['horariof']?></td>
-                        </tr>
-                      <?php endforeach; ?>        
-       </table>
-   </div>  
-</form>
-=======
+<div class="content">
+  <div class="card-body px-lg-1 pt-0">
+   <h2>AGREGAR HORARIO</h2> 
+     <form  class="text-center" method="POST"> 
+       <div class="table-responsive">
+           <table class="table">
+             <tr>
+              <th scope="col">DÍA</th>
+              <th scope="col">HORA DE INICIO</th>
+              <th scope="col">HORA DE FIN</th>
+             </tr>
+              <?php foreach ($horario as $a): ?>
+                <tr>
+                  <td><?php echo $a['dia']?></td>
+                  <td><?php echo $a['horarioi']?></td>
+                  <td><?php echo $a['horariof']?></td>
+                </tr>
+              <?php endforeach; ?>        
+           </table>
+       </div>  
+    </form>
+  </div> 
+
+
+
+</div>
+
+
+
                 <div class="table-responsive">
                     </tr>
                        <?php 
@@ -110,7 +124,7 @@ include("../controllers/horarioparaturista.php");
                          endforeach;
                         ?>
                 </div>  
->>>>>>> 08546a2c08ad59f5a09d173ba4932b38de87e5e2
+
 
 <!-- The Modal -->
   <div class="modal fade" id="myModal">
